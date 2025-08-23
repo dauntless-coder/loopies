d741de5867ea48418a9a3c0fa92ce632cbb6adc4
Aditi <Aditi@dauntlesscoder.com>
2025-08-23 16:45:22 +0530

    backend

---
+++ loopies-chatapp-backend/src/main/java/com/loopies/chat/controllers/ChatController.java
@@ -0,0 +1,4 @@
+package com.loopies.chat.controllers;
+
+public class ChatController {
+}
---
+++ loopies-chatapp-backend/src/test/java/com/loopies/chat/LoopiesChatappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopies.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopiesChatappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
